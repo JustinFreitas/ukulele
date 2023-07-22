deca64951acdbfb0281ede0dee82aabb90e075c2
JustinFreitas <justin.s.freitas@me.com>
2023-07-22 13:59:22 -0600

    Spring and Flyway updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     java
-    id("org.springframework.boot") version "3.1.1"
+    id("org.springframework.boot") version "3.1.2"
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.spring") version "1.9.0"
 }
@@ -21,7 +21,10 @@ repositories {
 
 dependencies {
     // Required for BotProps
-    implementation("org.springframework.boot:spring-boot-configuration-processor:3.1.1")
+    implementation("org.springframework.boot:spring-boot-configuration-processor:3.1.2")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc:3.1.2")
+    // https://mvnrepository.com/artifact/org.yaml/snakeyaml
+    implementation("org.yaml:snakeyaml:2.0")
 
     // https://mvnrepository.com/artifact/net.dv8tion/JDA
     implementation("net.dv8tion:JDA:5.0.0-beta.12")
@@ -43,13 +46,9 @@ dependencies {
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-annotations
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc:3.1.1")
-    // https://mvnrepository.com/artifact/org.yaml/snakeyaml
-    implementation("org.yaml:snakeyaml:2.0")
-
     runtimeOnly("com.h2database:h2:2.2.220")
     implementation("io.r2dbc:r2dbc-h2:1.0.0.RELEASE")
-    implementation("org.flywaydb:flyway-core:9.20.1")
+    implementation("org.flywaydb:flyway-core:9.21.0")
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.6")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
