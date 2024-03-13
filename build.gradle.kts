29aabcf7968b316a11a96cd4de06caae5e0af22c
JustinFreitas <justin.s.freitas@me.com>
2024-03-13 06:03:54 -0600

    Kotlinx Coroutines Reactor dep update

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib-jdk8
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-reactor
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.1-Beta")
     // https://mvnrepository.com/artifact/io.projectreactor.kotlin/reactor-kotlin-extensions
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.2.2")
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-test
