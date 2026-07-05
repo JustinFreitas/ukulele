3172678c346294a1c9720dfd9eb1ec3e752a2d2e
Justin Freitas <justin.s.freitas@me.com>
2026-07-04 18:35:37 -0600

    Migrate build dependencies and plugins to Version Catalog (libs.versions.toml)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,17 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    val kotlinVersion = "2.4.0"
     java
-    // https://plugins.gradle.org/plugin/org.springframework.boot
-    id("org.springframework.boot") version "4.1.0"
-    // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm
-    kotlin("jvm") version kotlinVersion
-    // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.spring
-    kotlin("plugin.spring") version kotlinVersion
-    // https://plugins.gradle.org/plugin/org.flywaydb.flyway
-    id("org.flywaydb.flyway") version "12.9.0"
-    // https://plugins.gradle.org/plugin/com.github.ben-manes.versions
-    id("com.github.ben-manes.versions") version "0.54.0"
-    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+    alias(libs.plugins.springboot)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.flyway)
+    alias(libs.plugins.benmanes)
+    alias(libs.plugins.ktlint)
 }
 
 ktlint {
-    version.set("1.8.0")
+    version.set(libs.versions.ktlintEngine)
 }
 
 tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
@@ -28,16 +22,6 @@ tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
         !isStable
     }
 }
-
-// Version Constants
-val kotlinVersion = "2.4.0"
-val springBootVersion = "4.1.0"
-val springFrameworkVersion = "7.0.7"
-val jacksonVersion = "3.1.2"
-// https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-annotations
-val jacksonAnnotationsVersion = "2.21"
-val nettyVersion = "4.2.15.Final"
-val jdaveVersion = "0.1.8"
 
 kotlin {
     jvmToolchain(25)
@@ -66,26 +50,26 @@ dependencies {
     // newer split modules (netty-codec-base, -http3, -classes-quic, ...) which used to mismatch the
     // modules we pinned by hand, producing a fat jar with both 4.2.12 and 4.2.15 on the classpath.
     // The BOM (enforced) guarantees one version across all modules, including ones added upstream.
-    implementation(enforcedPlatform("io.netty:netty-bom:$nettyVersion"))
+    implementation(enforcedPlatform(libs.netty.bom))
 
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-configuration-processor
-    implementation("org.springframework.boot:spring-boot-configuration-processor:$springBootVersion")
+    implementation(libs.springboot.configuration.processor)
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-r2dbc
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc:$springBootVersion")
+    implementation(libs.springboot.starter.data.r2dbc)
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-webflux
-    implementation("org.springframework.boot:spring-boot-starter-webflux:$springBootVersion")
+    implementation(libs.springboot.starter.webflux)
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
-    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+    implementation(libs.springboot.starter.web)
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-websocket
-    implementation("org.springframework.boot:spring-boot-starter-websocket:$springBootVersion")
+    implementation(libs.springboot.starter.websocket)
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-actuator
-    implementation("org.springframework.boot:spring-boot-starter-actuator:$springBootVersion")
+    implementation(libs.springboot.starter.actuator)
     // https://mvnrepository.com/artifact/org.yaml/snakeyaml
-    implementation("org.yaml:snakeyaml:2.6")
+    implementation(libs.snakeyaml)
 
     // https://mvnrepository.com/artifact/net.dv8tion/JDA
     // https://github.com/discord-jda/JDA
-    implementation("net.dv8tion:JDA:6.4.2")
+    implementation(libs.jda)
 
     // Jocull has the latest Ukulele we should pattern after in JDA-5 branch.
     // https://github.com/jocull/ukulele/tree/jda-5
@@ -98,13 +82,13 @@ dependencies {
     // empty manifest-only jar that collides with the real submodule jar by filename
     // in BOOT-INF/lib, so the bootJar classloader binds to the empty copy and fails
     // with ClassNotFoundException for AudioPlayerManager.
-    implementation("com.github.JustinFreitas.lavaplayer:lavaplayer:v2.2.6_20")
-    implementation("com.github.JustinFreitas.lavaplayer:lavaplayer-ext-format-xm:v2.2.6_20")
-    implementation("com.github.JustinFreitas.lavaplayer:lavaplayer-ext-youtube-rotator:v2.2.6_20")
+    implementation(libs.lavaplayer)
+    implementation(libs.lavaplayer.ext.format.xm)
+    implementation(libs.lavaplayer.ext.youtube.rotator)
     // https://mvnrepository.com/artifact/club.minnced/jdave-api
-    implementation("club.minnced:jdave-api:$jdaveVersion")
-    implementation("club.minnced:jdave-native-win-x86-64:$jdaveVersion")
-    implementation("club.minnced:jdave-native-linux-x86-64:$jdaveVersion")
+    implementation(libs.jdave.api)
+    implementation(libs.jdave.native.win)
+    implementation(libs.jdave.native.linux)
     // https://github.com/lavalink-devs/youtube-source
     // Justin's HttpClient-5 fork of youtube-source, rebuilt against the HC5 lavaplayer fork.
     // The upstream dev.lavalink.youtube:*:1.18.1 jars are compiled against HttpClient 4 and fail at
@@ -112,65 +96,65 @@ dependencies {
     //   AbstractMethodError: ... onContextOpen(org.apache.hc.client5.http.protocol.HttpClientContext)
     // https://github.com/JustinFreitas/youtube-source (tag v1.18.1_1)
     // The "youtube-plugin" module is the standalone Lavalink-server plugin and is unused here.
-    implementation("com.github.JustinFreitas.youtube-source:v2:v1.18.1_1")
-    implementation("com.github.JustinFreitas.youtube-source:common:v1.18.1_1")
+    implementation(libs.youtube.source.v2)
+    implementation(libs.youtube.source.common)
     // https://mvnrepository.com/artifact/commons-io/commons-io
-    implementation("commons-io:commons-io:2.21.0")
+    implementation(libs.commons.io)
     // https://mvnrepository.com/artifact/org.json/json
-    implementation("org.json:json:20251224")
+    implementation(libs.org.json)
     // https://mvnrepository.com/artifact/org.jsoup/jsoup
-    implementation("org.jsoup:jsoup:1.22.1")
+    implementation(libs.jsoup)
 
     // https://mvnrepository.com/artifact/com.google.code.gson/gson
-    implementation("com.google.code.gson:gson:2.13.2")
+    implementation(libs.gson)
 
     // https://mvnrepository.com/artifact/org.apache.httpcomponents.client5/httpclient5
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.6.1")
+    implementation(libs.httpclient5)
     // Explicit httpcore5 to access SocketConfig
-    implementation("org.apache.httpcomponents.core5:httpcore5:5.4.3")
+    implementation(libs.httpcore5)
 
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
-    implementation("tools.jackson.core:jackson-core:$jacksonVersion")
+    implementation(libs.jackson.core)
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
-    implementation("tools.jackson.core:jackson-databind:$jacksonVersion")
+    implementation(libs.jackson.databind)
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-annotations
-    implementation("com.fasterxml.jackson.core:jackson-annotations:$jacksonAnnotationsVersion")
+    implementation(libs.jackson.annotations)
 
     // https://mvnrepository.com/artifact/com.h2database/h2
     // NOTE: 2.4.240 caused a problem where Ukulele couldn't join the channel for some reason.
     // UPDATE: 20251213 - I think since both of these deps are updated now (2.4.240 and 1.1.0), we can try the DB update
     // again and see if we can get everything to work now.
-    runtimeOnly("com.h2database:h2:2.4.240")
+    runtimeOnly(libs.h2)
     // https://mvnrepository.com/artifact/io.r2dbc/r2dbc-h2
-    implementation("io.r2dbc:r2dbc-h2:1.1.0.RELEASE") {
+    implementation(libs.r2dbc.h2) {
         exclude(group = "com.h2database", module = "h2")
     }
     // https://mvnrepository.com/artifact/org.flywaydb/flyway-core
-    implementation("org.flywaydb:flyway-core:12.9.0")
+    implementation(libs.flyway.core)
     // https://mvnrepository.com/artifact/com.github.ben-manes.caffeine/caffeine
-    implementation("com.github.ben-manes.caffeine:caffeine:3.2.4")
+    implementation(libs.caffeine)
 
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-reflect
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation(libs.kotlin.reflect)
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib-jdk8
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation(libs.kotlin.stdlib.jdk8)
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-reactor
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.11.0")
+    implementation(libs.kotlinx.coroutines.reactor)
     // https://mvnrepository.com/artifact/io.projectreactor.kotlin/reactor-kotlin-extensions
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.3.1")
+    implementation(libs.reactor.kotlin.extensions)
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-test
-    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion") {
+    testImplementation(libs.springboot.starter.test) {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
 
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
-    testImplementation("org.junit.jupiter:junit-jupiter:5.14.3")
+    testImplementation(libs.junit.jupiter)
 }
 
 val mockitoAgent: Configuration by configurations.creating
 
 dependencies {
-    mockitoAgent("net.bytebuddy:byte-buddy-agent:1.18.10") {
+    mockitoAgent(libs.byte.buddy.agent) {
         isTransitive = false
     }
 }
